54ce77df48baf0144d46d4a2851c932bbc7cb997
emazack <em.zaccaria@gmail.com>
2022-01-29 19:01:56 +0100

    insert the preliminary css and component

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-import * as _ from 'lodash';
 
-  function component() {
-    const element = document.createElement('div');
 
-    element.innerHTML = _.join(['prova', 'webpack'], ' ');
+function component() {
+  const element = document.createElement('div');
+  element.classList.add("cashflow-trend-component");
+  
+  element.innerHTML = 
+  `
+  <div class="month-box">
+    <div class="name">
+      Gennaio
+    </div>
+    <div class="infos">
+      <div class="document">
+        <div class="count">
+          4
+        </div>
+        <div class="text">
+          doc.
+        </div>
+      </div>
+      <div class="money">
+        15.000 $
+      </div>
+    </div>
+  </div>
+  `;
+  
+  return element;
+}
 
-    return element;
-  }
-
-  document.body.appendChild(component());
+document.body.appendChild(component());
